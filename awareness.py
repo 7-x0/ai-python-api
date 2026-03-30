9f81098528627222e6de50db2e7746de1e568c0c
7-x0 <ni9ce25@gmail.com>
2026-03-30 14:00:19 +0300

    Create awareness.py

---
+++ awareness.py
@@ -0,0 +1,8 @@
+def track_event(mem, event):
+    if "events" not in mem:
+        mem["events"] = []
+
+    mem["events"].append(event)
+
+def get_recent_events(mem):
+    return mem.get("events", [])[-10:]
