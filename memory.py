7e9859fc3f97f462aaa5399d805ba5918752c8a1
7-x0 <ni9ce25@gmail.com>
2026-03-30 14:22:06 +0300

    Update memory.py

--- memory.py
+++ memory.py
@@ -5,22 +5,39 @@ FILE = "memory.json"
 def load():
     if not os.path.exists(FILE):
         return {
+            "global_users": {},
+            "guilds": {}
+        }
+    return json.load(open(FILE))
+
+
+def save(mem):
+    json.dump(mem, open(FILE, "w"), indent=2)
+
+
+def get_guild(mem, guild_id):
+    if guild_id not in mem["guilds"]:
+        mem["guilds"][guild_id] = {
             "users": {},
             "relations": [],
             "events": []
         }
-    return json.load(open(FILE))
+    return mem["guilds"][guild_id]
 
-def save(mem):
-    json.dump(mem, open(FILE,"w"), indent=2)
 
-def get_user(mem, uid, name):
-    return mem["users"].get(uid, {
-        "name": name,
+def get_user(guild, user_id, username):
+    return guild["users"].get(user_id, {
+        "name": username,
         "notes": [],
         "history": [],
         "mood": "neutral"
     })
 
-def store_event(mem, text):
-    mem["events"].append(text)
+
+def get_global_user(mem, user_id):
+    if user_id not in mem["global_users"]:
+        mem["global_users"][user_id] = {
+            "notes": [],
+            "history": []
+        }
+    return mem["global_users"][user_id]
